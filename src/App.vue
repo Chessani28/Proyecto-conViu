56f348f094c272f15f580c3a74fe2766c36dfc09
MexiV <ricardo@max4technologies.com>
2023-01-19 11:24:53 -0600

    ??????

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { ref, computed } from "vue";
+
+const name = "Vue dinámico";
+
+const counter = ref(0);
+const arrayFavoritos = ref([])
+
+
+const increment = () => {
+  counter.value++;
+};
+const decrement = () =>{
+ counter.value--;
+};
+const reset = () => {
+  counter.value = 0;
+};
+
+const add = () => {
+  arrayFavoritos.value.push(counter.value);
+};
+
+const bloquearBtnAdd = computed(() =>{
+  const numSearch = arrayFavoritos.value.find((num)=> num === counter.value);
+  console.log(numSearch);
+  //if (numSearch ===0) return true;
+  //return numSearch ? true : false;
+  return numSearch || numSearch === 0
+});
+
+
+const classCounter = computed(() => {
+  if(counter.value === 0){
+    return 'zero';
+  }
+  if(counter.value > 0){;
+    return 'positive';
+  }
+  if(counter.value < 0){
+    return 'negative';
+  }
+});
+</script>
+
+<template>
+  <div class="container text-center mt-3">
+  <h1> Hola {{ name.toUpperCase }}</h1>
+  <h2 :class="classCounter">{{ counter }}</h2>
+  <button @click="increment" class="btn btn-sucess">Incrementar</button>
+  <button @click="decrement" class="btn btn-danger">Decrement</button>
+  <button @click="reset" class="btn btn-secondary">Reset</button>
+  <button @click="add" :disabled="bloquearBtnAdd" class="btn btn-primary">
+    Add
+</button>
+  <br>
+  {{ arrayFavoritos }}
+  <ul class="list-group-item">
+    <li class ="list-group-item"
+      v-for="(num, index) in arrayFavoritos"
+      :key="index"
+    >
+    {{ num }}
+  </li>
+    </ul>
+    </div>
+</template>
+
+<style>
+h1{
+  color: red;
+}
+.positive{
+  color: green;
+}
+.negative{
+  color:blue;
+}
+.zero{
+  color: peru;
+}
+</style>
